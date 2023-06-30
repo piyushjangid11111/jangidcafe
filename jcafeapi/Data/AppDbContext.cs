6b32f52324e30aab03a3f52b20f19689a49534f5
piyushjangid11111 <130186212+piyushjangid11111@users.noreply.github.com>
2023-06-30 18:50:37 +0530

    Add files via upload

---
+++ jcafeapi/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using jangidcafeapi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace jangidcafeapi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Contact> contacts { get; set; }
+    }
+}
